96b06b79222c7a450de235507645c0b6c68b9a39
Joaquin Gimenez <joaquingim77@gmail.com>
2026-06-12 11:50:31 +0000

    add express middleware for JSON and URL-encoded request handling

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,9 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors();
+
+  app.use(express.json({ limit: '100mb' }));
+  app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
   app.useGlobalPipes(
     new ValidationPipe({
